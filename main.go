477af83690e388c9553397abd966ce2587d72d4b
Amir <bibarsov.amir@yandex.ru>
2019-05-07 14:14:32 +0300

    Шаблоны golang

--- main.go
+++ main.go
@@ -3,10 +3,30 @@ package main
 import (
     "fmt"
     "net/http"
+    "html/template"
+    //"github.com/gorilla/mux"
 )
 
 
+type ViewData struct{
+    Title string
+    Message string
+}
+
 func main(){
-    fmt.Println("Server is listening...")
-    http.ListenAndServe(":8181", http.FileServer(http.Dir("c:/users/admin/desktop/goworkspace/static")))
+
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
+        data := ViewData{
+            Title : "Champ Boris Britva",
+            Message : "Congratulations!!!",
+        }
+        tmpl, _ := template.ParseFiles("c:/users/admin/desktop/goworkspace/templates/userdie.html")
+        tmpl.Execute(w, data)
+        
+    })
+
+
+
+    fmt.Println("Server...")
+    http.ListenAndServe(":8181", nil)
 }
